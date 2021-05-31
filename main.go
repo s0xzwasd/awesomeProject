fab391d3d279199e8dd104a7b90799f3ca5c45fb
Daniil Maslov <i@dmaslov.dev>
2021-05-31 16:16:36 +0300

    Add a one more statement

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
+/*
+	#include <stdlib.h>
+	char* cstring = "C string example";
+*/
+import "C"
 import (
-	"flag"
 	"fmt"
+	"unsafe"
 )
 
 func main() {
-	str := flag.String("str", "", "")
-	flag.Parse()
-	print(*str)
-	fmt.Println("Hello, World!")
-	fmt.Println("Add one more")
+
+	var gstring string = "Go string example"
+
+	//Go to C String, Output: *C.char
+	cs := C.CString(gstring)
+	defer C.free(unsafe.Pointer(cs))
+	fmt.Println(cs)
+
+	//C to Go String, Output: string
+	gs := C.GoString(C.cstring)
+	fmt.Println(gs)
+
+	//C string, length to Go string
+	gs2 := C.GoStringN(C.cstring, (C.int)(len(gs)))
+	fmt.Println(gs2)
+
 }
